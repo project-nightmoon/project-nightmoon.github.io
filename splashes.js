c00a753a04118b94f2c1bc5ac7f2112b1ab1c9ec
goaty <159173523+1goaty1@users.noreply.github.com>
2024-09-29 07:10:42 -0700

    Update splashes.js

--- splashes.js
+++ splashes.js
@@ -1,4 +1,8 @@
 const splashes = [
   "Wanted by floppy!",
   "Marketplace is a scam",
+  "P2WIN is against Mojang Tos",
+  ".gg/nightmoon",
+  "Official Realm Support!",
+  "I just lost my dawg",
 ]
