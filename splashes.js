5732fd5672fb67d4e65fb3950ce3f2033f2ad205
goaty <159173523+1goaty1@users.noreply.github.com>
2025-04-07 21:49:45 +0200

    Delete splashes.js

--- splashes.js
+++
@@ -1,9 +0,0 @@
-const splashes = [
-  "Greetings from The Fifth Column!",
-  "Aras doesnt protect your Realm",
-  "Marketplace is a scam",
-  "P2WIN is against Mojang Tos",
-  ".gg/nightmoon",
-  "Official Realm Support!",
-  "I just lost my dawg",
-]
